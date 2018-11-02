1759eb7353a5ee9008204dbd10de0315ea667f87
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-11-02 14:15:50 -0700

    output card

--- TipCalculatorStarter/ViewController.swift
+++ TipCalculatorStarter/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var themeSwitch: UISwitch!
-    
+    @IBOutlet weak var inputCardView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +29,11 @@ class ViewController: UIViewController {
             print("switch toggled off")
         }
     }
+    
+    @IBAction func tipPercentageChanged(_ sender: UISegmentedControl) {
+    }
+    
+    
     
 }
 

befa3a883160894c00ab91746cdc6c1e59159120
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-11-01 22:07:17 -0700

    ibOutlet and action

--- TipCalculatorStarter/ViewController.swift
+++ TipCalculatorStarter/ViewController.swift
@@ -12,8 +12,23 @@ class ViewController: UIViewController {
 
     // MARK: - View Lifecycle
     
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    @IBAction func themeToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            print("switch toggled on")
+        } else {
+            print("switch toggled off")
+        }
+    }
+    
 }
 
